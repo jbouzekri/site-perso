6cedbd3797a315da7e552c11ddf451a3a4f3374b
Jonathan Bouzekri <jonathan.bouzekri@gmail.com>
2018-04-20 16:44:17 +0200

    nunjuck templates

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
@@ -10,38 +12,57 @@ module.exports = {
         rules: [
             {
                 test: /\.scss$/,
-                use: [
-                    { loader: "style-loader" }, // creates style nodes from JS strings
-                    { loader: "css-loader" }, // translates CSS into CommonJS
-                    { loader: "sass-loader" } // compiles Sass to CSS
-                ]
+                use: ExtractTextPlugin.extract({
+                    use: [
+                        { loader: "css-loader" },
+                        { loader: "sass-loader" }
+                    ]
+                })
             },
             {
                 test: /\.(png|svg|jpg|gif|ico)$/,
                 use: [
-                    'file-loader?name=[name].[ext]'
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]'
+                        }
+                    }
                 ]
+            },
+            {
+                test: /\.html$/,
+                exclude: /partials/,
+                use: [
+                        { loader: 'html-loader' },
+                        {
+                            loader: "nunjucks-html-loader",
+                            options: {
+                                searchPaths: [path.resolve(__dirname, 'src')],
+                                context: {
+                                    username: 'titi'
+                                }
+                            }
+                        },
+                    ]
             }
         ]
     },
     plugins: [
         new CleanWebpackPlugin(['dist']),
+        new ExtractTextPlugin('style.css'),
         new HtmlWebpackPlugin({
             filename: 'index.html',
-            template: 'src/index.html'
+            template: './src/index.html',
         }),
         new HtmlWebpackPlugin({
             filename: 'cv.html',
-            template: 'src/cv.html'
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'contact.html',
-            template: 'src/contact.html'
+            template: './src/cv.html',
         })
     ],
     output: {
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist/')
     },
     devServer: {
         contentBase: path.join(__dirname, "dist"),
